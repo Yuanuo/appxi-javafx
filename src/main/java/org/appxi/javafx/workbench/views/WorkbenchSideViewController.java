d559b3232948c90da5972f2417aacd8cf8883586
Yuanuox <yuanuox@outlook.com>
2021-03-17 18:53:51 +0800

    修改SideView默认不带Toolbar

--- src/main/java/org/appxi/javafx/workbench/views/WorkbenchSideViewController.java
+++ src/main/java/org/appxi/javafx/workbench/views/WorkbenchSideViewController.java
@@ -3,17 +3,14 @@ package org.appxi.javafx.workbench.views;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import org.appxi.javafx.control.ToolBarEx;
 import org.appxi.javafx.workbench.WorkbenchApplication;
 import org.appxi.javafx.workbench.WorkbenchViewController;
 
 public abstract class WorkbenchSideViewController extends WorkbenchViewController {
     protected BorderPane viewport;
     protected VBox viewportVBox;
-    protected ToolBarEx toolbar;
-    protected Label titleBar;
+    protected HBox headBar;
 
     public WorkbenchSideViewController(String viewId, WorkbenchApplication application) {
         super(viewId, application);
@@ -24,17 +21,13 @@ public abstract class WorkbenchSideViewController extends WorkbenchViewControlle
         if (null == this.viewport) {
             this.viewport = new BorderPane();
 
-            this.titleBar = new Label();
-            this.titleBar.getStyleClass().add("title-bar");
-            this.titleBar.textProperty().bind(viewTitle);
+            final Label titleBar = new Label();
+            titleBar.getStyleClass().add("title-bar");
+            titleBar.textProperty().bind(viewTitle);
 
-            final HBox headBar = new HBox(titleBar);
-            headBar.getStyleClass().add("head-bar");
+            this.headBar = new HBox(titleBar);
+            this.headBar.getStyleClass().add("head-bar");
             this.viewport.setTop(headBar);
-
-            this.toolbar = new ToolBarEx();
-            HBox.setHgrow(this.toolbar, Priority.ALWAYS);
-            headBar.getChildren().add(this.toolbar);
 
             this.viewportVBox = new VBox();
             this.viewportVBox.getStyleClass().addAll("vbox", "side-vbox");
