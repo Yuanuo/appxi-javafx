6b7895b1c5365ed7c5bed2d35b90adc1766d2a8f
Yuanuox <yuanuox@outlook.com>
2022-06-09 11:08:41 +0800

    重构

---
+++ src/main/java/org/appxi/javafx/app/web/WebRendererPart.java
@@ -0,0 +1,101 @@
+package org.appxi.javafx.app.web;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import org.appxi.javafx.workbench.WorkbenchPane;
+import org.appxi.javafx.workbench.WorkbenchPart;
+
+public abstract class WebRendererPart extends WebRenderer implements WorkbenchPart {
+    private static final Object AK_INITIALIZED = new Object();
+
+    public final StringProperty id = new SimpleStringProperty();
+    public final StringProperty title = new SimpleStringProperty();
+    public final StringProperty tooltip = new SimpleStringProperty();
+    public final ObjectProperty<Node> graphic = new SimpleObjectProperty<>();
+
+    public WebRendererPart(WorkbenchPane workbench, StackPane viewport) {
+        super(workbench, viewport);
+    }
+
+    @Override
+    public final StringProperty id() {
+        return id;
+    }
+
+    @Override
+    public final StringProperty title() {
+        return title;
+    }
+
+    @Override
+    public final StringProperty tooltip() {
+        return tooltip;
+    }
+
+    @Override
+    public final ObjectProperty<Node> graphic() {
+        return graphic;
+    }
+
+    public static abstract class SideView extends WebRendererPart implements WorkbenchPart.SideView {
+        public SideView(WorkbenchPane workbench, StackPane viewport) {
+            super(workbench, viewport);
+        }
+
+        @Override
+        public final StackPane getViewport() {
+            if (!viewport.getProperties().containsKey(AK_INITIALIZED)) {
+                viewport.getProperties().put(AK_INITIALIZED, true);
+                install();
+            }
+            return viewport;
+        }
+
+        @Override
+        public void activeViewport(boolean firstTime) {
+            if (firstTime) {
+                navigate(null);
+            }
+        }
+    }
+
+    public static abstract class MainView extends WebRendererPart implements WorkbenchPart.MainView {
+        public final StringProperty appTitle = new SimpleStringProperty();
+
+        public MainView(WorkbenchPane workbench, StackPane viewport) {
+            super(workbench, viewport);
+        }
+
+        @Override
+        public final StringProperty appTitle() {
+            return appTitle;
+        }
+
+        @Override
+        public final StackPane getViewport() {
+            if (!viewport.getProperties().containsKey(AK_INITIALIZED)) {
+                viewport.getProperties().put(AK_INITIALIZED, true);
+                install();
+            }
+            return viewport;
+        }
+
+        @Override
+        public void activeViewport(boolean firstTime) {
+            if (firstTime) {
+                navigate(null);
+            }
+        }
+
+        @Override
+        public void inactiveViewport(boolean closing) {
+            if (closing) {
+                uninstall();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/appxi/javafx/app/web/WebViewerPart.java
@@ -0,0 +1,108 @@
+package org.appxi.javafx.app.web;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import org.appxi.javafx.workbench.WorkbenchPane;
+import org.appxi.javafx.workbench.WorkbenchPart;
+
+public abstract class WebViewerPart extends WebViewer implements WorkbenchPart {
+    private static final Object AK_INITIALIZED = new Object();
+
+    public final StringProperty id = new SimpleStringProperty();
+    public final StringProperty title = new SimpleStringProperty();
+    public final StringProperty tooltip = new SimpleStringProperty();
+    public final ObjectProperty<Node> graphic = new SimpleObjectProperty<>();
+
+    public WebViewerPart(WorkbenchPane workbench, StackPane viewport) {
+        super(workbench, viewport);
+    }
+
+    @Override
+    public final StringProperty id() {
+        return id;
+    }
+
+    @Override
+    public final StringProperty title() {
+        return title;
+    }
+
+    @Override
+    public final StringProperty tooltip() {
+        return tooltip;
+    }
+
+    @Override
+    public final ObjectProperty<Node> graphic() {
+        return graphic;
+    }
+
+    public static abstract class SideView extends WebViewerPart implements WorkbenchPart.SideView {
+        public SideView(WorkbenchPane workbench, StackPane viewport) {
+            super(workbench, viewport);
+        }
+
+        @Override
+        public final StackPane getViewport() {
+            if (!viewport.getProperties().containsKey(AK_INITIALIZED)) {
+                viewport.getProperties().put(AK_INITIALIZED, true);
+                install();
+            }
+            return viewport;
+        }
+
+        @Override
+        public void activeViewport(boolean firstTime) {
+            if (firstTime) {
+                navigate(null);
+            }
+        }
+
+        @Override
+        public void inactiveViewport() {
+            saveUserData();
+        }
+    }
+
+    public static abstract class MainView extends WebViewerPart implements WorkbenchPart.MainView {
+        public final StringProperty appTitle = new SimpleStringProperty();
+
+        public MainView(WorkbenchPane workbench, StackPane viewport) {
+            super(workbench, viewport);
+        }
+
+        @Override
+        public final StringProperty appTitle() {
+            return appTitle;
+        }
+
+        @Override
+        public final StackPane getViewport() {
+            if (!viewport.getProperties().containsKey(AK_INITIALIZED)) {
+                viewport.getProperties().put(AK_INITIALIZED, true);
+                install();
+            }
+            return viewport;
+        }
+
+        @Override
+        public void activeViewport(boolean firstTime) {
+            if (firstTime) {
+                navigate(null);
+            }
+        }
+
+        @Override
+        public void inactiveViewport(boolean closing) {
+            if (closing) {
+                uninstall();
+            } else {
+                saveUserData();
+            }
+        }
+    }
+}
